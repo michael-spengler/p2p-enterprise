e766501fa8e8c2027b8f8b3d79e042ccef77da31
michael-spengler <mycamper@gmx.de>
2020-10-14 21:19:06 +0200

    link to repo

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -6,16 +6,26 @@
       <!-- <source src="movie.ogg" type="video/ogg"> -->
       Your browser does not support the video tag.
     </video>
+
+    <h2>
+      Feel free to fork the following repo and create a PR to contribute:
+    </h2> 
+    <br />
+    <a
+      href="https://github.com/michael-spengler/p2p-enterprise"
+      target="_blank"
+      rel="noopener noreferrer"
+    >https://github.com/michael-spengler/p2p-enterprise</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
